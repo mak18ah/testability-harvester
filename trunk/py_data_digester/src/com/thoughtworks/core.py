7f5cc17bffa9c1e67405a929684e608a58e2e764
isaachanstar <isaachanstar@3ac56e84-c0a9-11dd-876f-61a20fcdc92d>
2009-01-15 00:46:25 +0000

    comment 'logger' feature

--- trunk/py_data_digester/src/com/thoughtworks/core.py
+++ trunk/py_data_digester/src/com/thoughtworks/core.py
@@ -1,5 +1,5 @@
 import os
-from com.thoughtworks.logger import *
+#from com.thoughtworks.logger import *
 
 class Project:
     
@@ -11,17 +11,17 @@ class Project:
         self.path = path
         self.changeSets = []
         
-        logger.info("Project Initialize - id: " + str(self.id))
-        logger.info("Project Initialize - name: " + self.name)
-        logger.info("Project Initialize - path: " + self.path)
-        logger.info("Project Initialize - developersL: " + self.developers)
+        #logger.info("Project Initialize - id: " + str(self.id))
+        #logger.info("Project Initialize - name: " + self.name)
+        #logger.info("Project Initialize - path: " + self.path)
+        #logger.info("Project Initialize - developersL: " + self.developers)
         
     def fetchChangeSets(self):
         changeSets = self.scmReader.changeSets(self.path)
         for cs in changeSets:
             cs.fetchFiles(self.scmReader)
             cs.path = self.path
-            logger.info("Project.fetchChangeSet - path: " + cs.path)
+            #logger.info("Project.fetchChangeSet - path: " + cs.path)
         self.changeSets = changeSets
         
     def toCSV(self):
@@ -29,7 +29,7 @@ class Project:
         for cs in self.changeSets:
             csv += cs.toCSV() + '\n'
         
-        logger.info("Project.toCSV - csv: " + csv)
+        #logger.info("Project.toCSV - csv: " + csv)
         return csv
     
     
@@ -42,10 +42,10 @@ class ChangeSet:
         self.files = files
         self.path = ''
         
-        logger.info("ChangeSet Initialize - number: " + str(self.number))
-        logger.info("ChangeSet Initialize - author: " + self.author)
-        logger.info("ChangeSet Initialize - date: " + self.date)
-        logger.info("ChangeSet Initialize - files: " + str(self.files))
+        #logger.info("ChangeSet Initialize - number: " + str(self.number))
+        #logger.info("ChangeSet Initialize - author: " + self.author)
+        #logger.info("ChangeSet Initialize - date: " + self.date)
+        #logger.info("ChangeSet Initialize - files: " + str(self.files))
     
     def fetchFiles(self, scmReader):
         for file in self.files:
@@ -56,7 +56,7 @@ class ChangeSet:
         csv = str(self.number) + ',' + self.author + ',' + \
                self.csvLanguage() + ',' + self.csvDate() + ',' + \
                'null' + ',' + self.csvAffectedLines()
-        logger.info("ChangeSet.toCSV - csv: " + csv)
+        #logger.info("ChangeSet.toCSV - csv: " + csv)
         return csv
     
     def csvLanguage(self):
@@ -83,7 +83,7 @@ class ChangeSet:
                 tD += f.countDeletedLines()
                 tM += f.countModifiedLines()
         statistics = str(pA) + ',' + str(pM) + ',' + str(pD) + ',' + str(tA) + ',' + str(tM) + ',' + str(tD)
-        logger.info("ChangeSet.csvAffectedLines - statistics: " + statistics)
+        #logger.info("ChangeSet.csvAffectedLines - statistics: " + statistics)
         return statistics
          
 
@@ -126,9 +126,9 @@ class AddedFile(File):
         File.__init__(self, path)
         self.content = ''
         
-        logger.info("AddedFile Initialize - path: " + self.path)
-        logger.info("AddedFile Initialize - type: " + self.type)
-        logger.info("AddedFile Initialize - language: " + self.language)
+        #logger.info("AddedFile Initialize - path: " + self.path)
+        #logger.info("AddedFile Initialize - type: " + self.type)
+        #logger.info("AddedFile Initialize - language: " + self.language)
     
     def fetch(self, changeSet, scmReader):
         content = scmReader.cat(changeSet.path, self.path, changeSet.number)
@@ -144,9 +144,9 @@ class DeletedFile(File):
         File.__init__(self, path)
         self.content = ''
         
-        logger.info("DeletedFile Initialize - path: " + self.path)
-        logger.info("DeletedFile Initialize - type: " + self.type)
-        logger.info("DeletedFile Initialize - language: " + self.language)
+        #logger.info("DeletedFile Initialize - path: " + self.path)
+        #logger.info("DeletedFile Initialize - type: " + self.type)
+        #logger.info("DeletedFile Initialize - language: " + self.language)
     
     def fetch(self, changeSet, scmReader):
         content = scmReader.cat(changeSet.path, self.path, changeSet.number - 1)
@@ -162,9 +162,9 @@ class ModifiedFile(File):
         File.__init__(self, path)
         self.diff = None
     
-        logger.info("ModifiedFile Initialize - path: " + self.path)
-        logger.info("ModifiedFile Initialize - type: " + self.type)
-        logger.info("ModifiedFile Initialize - language: " + self.language)
+        #logger.info("ModifiedFile Initialize - path: " + self.path)
+        #logger.info("ModifiedFile Initialize - type: " + self.type)
+        #logger.info("ModifiedFile Initialize - language: " + self.language)
         
     def fetch(self, changeSet, scmReader):
         self.diff = scmReader.diff(changeSet.path, self.path, changeSet.number- 1, changeSet.number)
@@ -207,15 +207,15 @@ class Diff:
                 suspicion = False 
         
     def countAddedLines(self):
-        logger.info("Diff.countAddedLines - added lines: " + str(self.__added))
+        #logger.info("Diff.countAddedLines - added lines: " + str(self.__added))
         return self.__added
     
     def countDeletedLines(self):
-        logger.info("Diff.countDeletedLines - deleted lines: " + str(self.__deleted))
+        #logger.info("Diff.countDeletedLines - deleted lines: " + str(self.__deleted))
         return self.__deleted
     
     def countModifiedLines(self):
-        logger.info("Diff.countModifiedLines - modified lines: " + str(self.__modified))
+        #logger.info("Diff.countModifiedLines - modified lines: " + str(self.__modified))
         return self.__modified    
 
 
--- trunk/py_data_digester/src/demo.py
+++ trunk/py_data_digester/src/demo.py
@@ -5,6 +5,7 @@ from com.thoughtworks.scmreader import *
 # Mock SCMReader for testing
 ################################################
 class MockSCMReader(SCMReader):
+    
     def changeSets(self, proPath):
         files = []
         files.append(AddedFile('src/TestCase.java'))
@@ -55,25 +56,25 @@ path:http://svn-digester.googlecode.com/svn"""
 hId = application.saveProject(projectString1)
 svdId = application.saveProject(projectString2)
 
-logger.info('Two projects have been saved.')
-logger.info('    Project 1 named: ' + application.find(0).name)
-logger.info('    Project 2 named: ' + application.find(1).name)
+print 'Two projects have been saved.'
+print '    Project 1 named: ' + application.find(0).name
+print '    Project 2 named: ' + application.find(0).name
 
-logger.info('==========Get All Projects Information:============')
-logger.info(application.projects())
-logger.info('===================================================')
+print '==========Get All Projects Information:============'
+print application.projects()
+print '==================================================='
 
-logger.info('Update first project name to T-H')
+print 'Update first project name to T-H'
 application.updateProject(projectString11, hId)
 
-logger.info('======Get Updated All Projects Information:========')
-logger.info(application.projects())
-logger.info('===================================================')
+print '======Get Updated All Projects Information:========'
+print application.projects()
+print '==================================================='
 
-logger.info('!!!!!Get CSV of Harvester!!!!!')
-logger.info(application.toCSV(hId))
+print '!!!!!Get CSV of Harvester!!!!!'
+print application.toCSV(hId)
 
-logger.info('!!!!!Get CSV of svn-digester!!!!!')
-logger.info(application.toCSV(svdId))
+print '!!!!!Get CSV of svn-digester!!!!!'
+print application.toCSV(svdId)
 
 
