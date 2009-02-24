38b81f816e60ede67d755b22837b4dff94bb156f
isaachanstar <isaachanstar@3ac56e84-c0a9-11dd-876f-61a20fcdc92d>
2009-02-24 13:54:57 +0000

    * Add one new type: ProjectsStroge.   Adding a project is a asynchronization operation without waiting for fetching changeset information.

--- trunk/py_data_digester/src/com/thoughtworks/application.py
+++ trunk/py_data_digester/src/com/thoughtworks/application.py
@@ -1,3 +1,6 @@
+import thread
+import time
+from threading import *
 from com.thoughtworks.core import *
 
 class Application:
@@ -25,7 +28,7 @@ class Application:
         
         self.map[p.id] = p
         return p.id 
-    
+        
     def updateProject(self, projectString, id):
         project = deserialize(projectString)
         p = self.map[id]
@@ -42,3 +45,37 @@ class Application:
         for i in self.map:
             if id <= i: id = i
         return id + 1
+
+class ProjectsStorage(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self._run = True
+        self.projects = []
+        self.failedProjects = []
+        self.pendingProjects = []
+        self.start()
+
+    def addProject(self, pro):
+        self.pendingProjects.append(pro)
+        
+    def run(self):
+        while(self._run):
+            if len(self.pendingProjects) > 0:
+                self.process()
+            else:
+                time.sleep(1)
+            
+    def process(self):
+        while (len(self.pendingProjects) > 0 and self._run):
+            currPro = self.pendingProjects.pop(0)
+            try:
+                currPro.fetchChangeSets()
+                self.projects.append(currPro)
+            except:
+                self.failedProjects.append(currPro)
+
+    def stop(self):    
+        self._run = False
+        self = None
+        
+        
